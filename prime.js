2fb01c78122360c1ce73eefdd041ab38964788b5
yashuvaishnav <yashurishu22@gmail.com>
2023-02-16 00:52:30 +0530

    prime problem solved

--- prime.js
+++ prime.js
@@ -1,12 +1,13 @@
-let a = 10;
-let count =0 ;
-for (i=0;i<=a;i++){
-if (i%a!=0){
+let a = 5;
+let count =0;
+for (i=1;i<=a;i++){
+if (a%i==0){
 count++;
 }
 }
 if (count==2){
 console.log("prime");
 }
-else { console.log("not prime");
+else {
+console.log("Not prime")
 }
